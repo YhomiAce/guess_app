219b90e693f4cc916a442c296e89fe7c52518d85
YhomiAce <kareemyomi91@gmail.com>
2023-02-25 21:08:17 +0100

    platform and layouts

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView, View } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
@@ -61,10 +61,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title="Guess a Numbersssss" />
       {content}
-    </View>
+    </SafeAreaView>
   );
 }
 
